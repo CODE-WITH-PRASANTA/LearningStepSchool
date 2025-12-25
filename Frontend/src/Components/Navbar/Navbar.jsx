3ee7d3c6df67c72beca20d7f8508d2cb6eabac98
SarojKumarMallik <sarojkumarmallik94@gmail.com>
2025-12-25 18:38:03 +0530

    Home Page

--- Frontend/src/Components/Navbar/Navbar.jsx
+++ Frontend/src/Components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./Navbar.css";
+import logo from '../../assets/Logo.png'
 import {
   FaFacebookF,
   FaTwitter,
@@ -20,65 +21,58 @@ const Navbar = () => {
 
   return (
     <>
-      {/* ===== Top Info Bar ===== */}
-      <div className="top-bar">
-        <div className="top-left">
+      {/* ===== Top Bar ===== */}
+      <div className="Navbar-top-bar">
+        <div className="Navbar-top-left">
           <span><FaMapMarkerAlt /> Bhubaneswar, Odisha</span>
           <span><FaEnvelope /> info@schoolname.edu.in</span>
         </div>
 
-        <div className="top-right">
+        <div className="Navbar-top-right">
           <span>Follow:</span>
-          <FaFacebookF />
-          <FaTwitter />
-          <FaLinkedinIn />
-          <FaYoutube />
+          <FaFacebookF className="Navbar-social-icon" />
+          <FaTwitter className="Navbar-social-icon" />
+          <FaLinkedinIn className="Navbar-social-icon" />
+          <FaYoutube className="Navbar-social-icon" />
         </div>
       </div>
 
-      {/* ===== Main Navbar ===== */}
-      <nav className="navbar">
-        {/* Logo */}
-        <div className="logo">
+      {/* ===== Navbar ===== */}
+      <nav className="Navbar-navbar">
+        <div className="Navbar-logo">
           <img
-            src="https://kidsa.vercel.app/_next/static/media/logo.5f815319.svg"
+            src={logo}
             alt="School Logo"
           />
         </div>
 
-        {/* ===== Desktop Menu ===== */}
-        <ul className="nav-links">
-
+        <ul className="Navbar-nav-links">
           <li>Home</li>
 
-          {/* About */}
           <li
-            className="dropdown"
             onMouseEnter={() => setAboutOpen(true)}
             onMouseLeave={() => setAboutOpen(false)}
           >
             About Us <FaChevronDown />
-            <div className={`dropdown-menu ${aboutOpen ? "open" : ""}`}>
-              <span className="dropdown-item">About School</span>
-              <span className="dropdown-item">Principal’s Message</span>
-              <span className="dropdown-item">Vision & Mission</span>
-              <span className="dropdown-item">Infrastructure</span>
+            <div className={`Navbar-dropdown-menu ${aboutOpen ? "open" : ""}`}>
+              <span className="Navbar-dropdown-item">About School</span>
+              <span className="Navbar-dropdown-item">Principal’s Message</span>
+              <span className="Navbar-dropdown-item">Vision & Mission</span>
+              <span className="Navbar-dropdown-item">Infrastructure</span>
             </div>
           </li>
 
-          {/* Academics */}
           <li
-            className="dropdown"
             onMouseEnter={() => setAcademicOpen(true)}
             onMouseLeave={() => setAcademicOpen(false)}
           >
             Academics <FaChevronDown />
-            <div className={`dropdown-menu ${academicOpen ? "open" : ""}`}>
-              <span className="dropdown-item">Pre-Primary</span>
-              <span className="dropdown-item">Primary</span>
-              <span className="dropdown-item">Secondary</span>
-              <span className="dropdown-item">Academic Calendar</span>
-              <span className="dropdown-item">Examination System</span>
+            <div className={`Navbar-dropdown-menu ${academicOpen ? "open" : ""}`}>
+              <span className="Navbar-dropdown-item">Pre-Primary</span>
+              <span className="Navbar-dropdown-item">Primary</span>
+              <span className="Navbar-dropdown-item">Secondary</span>
+              <span className="Navbar-dropdown-item">Academic Calendar</span>
+              <span className="Navbar-dropdown-item">Examination System</span>
             </div>
           </li>
 
@@ -89,10 +83,9 @@ const Navbar = () => {
           <li>Contact</li>
         </ul>
 
-        {/* ===== Right Actions ===== */}
-        <div className="nav-actions">
-          <div className="call-box">
-            <div className="call-icon">
+        <div className="Navbar-nav-actions">
+          <div className="Navbar-call-box">
+            <div className="Navbar-call-icon">
               <FaPhoneAlt />
             </div>
             <div>
@@ -101,26 +94,28 @@ const Navbar = () => {
             </div>
           </div>
 
-          <button className="visit-btn">Apply Admission</button>
+          <button className="Navbar-visit-btn">Apply Admission</button>
 
-          {/* Mobile Hamburger */}
-          <div className="hamburger" onClick={() => setDrawerOpen(true)}>
+          <div
+            className="Navbar-hamburger"
+            onClick={() => setDrawerOpen(true)}
+          >
             <FaBars />
           </div>
         </div>
       </nav>
 
       {/* ===== Mobile Drawer ===== */}
-      <div className={`mobile-drawer ${drawerOpen ? "open" : ""}`}>
-        <div className="drawer-header">
+      <div className={`Navbar-mobile-drawer ${drawerOpen ? "open" : ""}`}>
+        <div className="Navbar-drawer-header">
           <img
-            src="https://kidsa.vercel.app/_next/static/media/logo.5f815319.svg"
+            src={logo}
             alt="School Logo"
           />
           <FaTimes onClick={() => setDrawerOpen(false)} />
         </div>
 
-        <ul className="drawer-menu">
+        <ul className="Navbar-drawer-menu">
           <li>Home</li>
           <li>About Us</li>
           <li>Academics</li>
@@ -129,15 +124,14 @@ const Navbar = () => {
           <li>Notices</li>
           <li>News</li>
           <li>Contact</li>
-          <li className="drawer-login">Login</li>
         </ul>
 
-        <div className="drawer-contact">
+        <div className="Navbar-drawer-contact">
           <p><FaMapMarkerAlt /> Bhubaneswar, Odisha</p>
           <p><FaEnvelope /> info@schoolname.edu.in</p>
           <p><FaPhoneAlt /> 8117048317</p>
 
-          <div className="drawer-socials">
+          <div className="Navbar-drawer-socials">
             <FaFacebookF />
             <FaTwitter />
             <FaYoutube />
@@ -147,7 +141,10 @@ const Navbar = () => {
       </div>
 
       {drawerOpen && (
-        <div className="overlay" onClick={() => setDrawerOpen(false)} />
+        <div
+          className="Navbar-overlay"
+          onClick={() => setDrawerOpen(false)}
+        />
       )}
     </>
   );
