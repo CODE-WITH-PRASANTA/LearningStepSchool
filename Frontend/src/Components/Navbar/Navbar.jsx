78df263d5012963c330606463bd405a9d7932fc4
Prasanta Kumar Khuntia <prasantakhuntia2018@gmail.com>
2025-12-26 00:15:31 +0530

    Footer , Contact , Breadcrum

--- Frontend/src/Components/Navbar/Navbar.jsx
+++ Frontend/src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
-import logo from '../../assets/Logo.png'
+import logo from '../../assets/Learning Step Logo.png'
 import {
   FaFacebookF,
   FaTwitter,
