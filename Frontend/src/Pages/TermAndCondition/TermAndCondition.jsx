371c6a981816590911538e65516d256fe1fd0fc9
swarnaprava-06032005 <150228737+swarnaprava-06032005@users.noreply.github.com>
2026-01-07 13:09:38 +0530

    Privacy Policy and Term & Condition completed

---
+++ Frontend/src/Pages/TermAndCondition/TermAndCondition.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import TermAndCondnBreadCrum from '../../Components/TermAndCondnBreadCrum/TermAndCondnBreadCrum'
+import TermAndCondnContent from '../../Components/TermAndCondnContent/TermAndCondnContent'
+const TermAndCondition = () => {
+  return (
+    <div>
+      <TermAndCondnBreadCrum/>
+      <TermAndCondnContent/>
+    </div>
+  )
+}
+
+export default TermAndCondition
