6e104177d346b3dbcd21660dd686dfdb45aceb54
ganeshpanda744-jpg <ganeshpanda744@gmail.com>
2026-01-07 11:03:00 +0530

    admission page complete

--- Frontend/src/Pages/Admissions/Admissions.jsx
+++ Frontend/src/Pages/Admissions/Admissions.jsx
@@ -2,14 +2,22 @@ import React from "react";
 import AdmissionsBreadcrumb from "../../Components/AdmissionsBreadcrumb/AdmissionsBreadcrumb";
 import EnrollWithUs from "../../Components/EnrollWithUs/EnrollWithUs";
 import SchoolFee from "../../Components/SchoolFee/SchoolFee";
+import HowToApplySection from "../../Components/HowToApplySection/HowToApplySection";
+import KnowAndAid from "../../Components/KnowAndAid/KnowAndAid";
+import Features from "../../Components/Features/Features";
 import FeesFAQ from "../../Components/FeesFAQ/FeesFAQ";
+import AdmissionFaqSection from "../../Components/AdmissionFaqSection/AdmissionFaqSection";
 const Admissions = () => {
   return (
     <div>
       <AdmissionsBreadcrumb />
       <EnrollWithUs />
       <SchoolFee />
+      <HowToApplySection />
+      <KnowAndAid />
+      <Features />
       <FeesFAQ />
+      <AdmissionFaqSection />
     </div>
   );
 };
