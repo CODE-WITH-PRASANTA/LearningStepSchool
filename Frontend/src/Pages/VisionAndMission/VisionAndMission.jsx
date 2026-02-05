0af5d274406dc101b182d4c02b26cefcd4113aaf
PRANGYA PARAMITA <prangyachoudhury56@gmail.com>
2026-02-05 14:33:47 +0530

    clswise management system

--- Frontend/src/Pages/VisionAndMission/VisionAndMission.jsx
+++ Frontend/src/Pages/VisionAndMission/VisionAndMission.jsx
@@ -7,16 +7,12 @@ import VMVideoHero from '../../Components/VMVideoHero/VMVideoHero'
 import VMMeetTeacher from '../../Components/VMMeetTeacher/VMMeetTeacher'
 import HighQualityBlendVisionMission from '../../Components/HighQualityBlendVisionMission/HighQualityBlendVisionMission'
 import VMSlider from '../../Components/VMSlider/VMSlider'
-<<<<<<< HEAD
 import VMFAQ from '../../Components/VMFAQ/VMFAQ' 
 import ContactForm from '../../Components/ContactForm/ContactForm'
 
 
 
-=======
 import MVKidesActivities from '../../Components/MVKidesActivities/MVKidesActivities'
-import VMFAQ from '../../Components/VMFAQ/VMFAQ'
->>>>>>> cd194cad08d4dc6026be74406b3bc38a091f6d99
 const VisionAndMission = () => {
   return (
     <div>
