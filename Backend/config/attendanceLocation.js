52cab095e38ff6cb96b188380b08ec6a06eea77e
Debashish mallick <mdebashish543@gmail.com>
2026-07-20 21:42:45 +0530

    ok

--- Backend/config/attendanceLocation.js
+++ Backend/config/attendanceLocation.js
@@ -1,11 +1,11 @@
 const ATTENDANCE_LOCATION = {
   name: "School Campus",
 
-  //   latitude: 20.1644,
-  latitude: 27.243927549411936,
+    latitude: 20.1644,
+//   latitude: 27.243927549411936,
 
-  //   longitude: 85.6981,
-  longitude: 76.59763715644159,
+    longitude: 85.6981,
+//   longitude: 76.59763715644159,
 
   radiusMeters: 400,
 };
